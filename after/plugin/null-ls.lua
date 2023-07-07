3c635cfde9f327045f66c9769d806ea87795d87e
Jefferson Chen <f1jefferson@yahoo.com.tw>
2023-07-07 16:54:51 +0800

    null-ls setup

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -2,9 +2,18 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
   sources = {
+    -- json
+    null_ls.builtins.formatting.json_tool,
+    -- python
     null_ls.builtins.formatting.black,
+    -- lua
     null_ls.builtins.formatting.stylua,
+    -- eslint
+    null_ls.builtins.formatting.eslint,
     null_ls.builtins.diagnostics.eslint,
+    -- prettier
+    null_ls.builtins.formatting.prettier,
+    --
     null_ls.builtins.completion.spell,
   },
 })
