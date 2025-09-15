55e0e07d6e2cd6ee24aa66a5f9ef047b2c0fd295
y1lichen <f1jefferson2@gmail.com>
2025-09-15 18:00:54 +0800

    add text-object

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,14 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
+
+  {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("config.nvim-treesitter-textobjects")
+		end,
+	},
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
